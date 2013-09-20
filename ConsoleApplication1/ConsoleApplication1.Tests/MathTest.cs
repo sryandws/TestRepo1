7d8acc5ec5a65065a7f9cf352ad5fd19010c5e98
Stephen Ryan <stephen.ryan@dws.com.au>
2013-09-20 12:08:12 +1000

    More unit tests

--- ConsoleApplication1/ConsoleApplication1.Tests/MathTest.cs
+++ ConsoleApplication1/ConsoleApplication1.Tests/MathTest.cs
@@ -17,6 +17,17 @@ namespace ConsoleApplication1.Tests
         }
 
         [TestMethod]
+        public void GivenAdd_WhenNegativeFivePlusTwo_ThenNegativeThree()
+        {
+            var target = new Math();
+
+            int expected = -3;
+            int actual = target.Add(-5, 2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
         public void GivenSubtract_WhenThreeMinusTwo_ThenOne()
         {
             var target = new Math();
@@ -25,6 +36,17 @@ namespace ConsoleApplication1.Tests
             int actual = target.Subtract(3, 2);
 
             Assert.AreEqual(expected, actual);
-        }    
+        }
+
+        [TestMethod]
+        public void GivenSubtract_WhenThreeMinusEight_ThenNegativeFive()
+        {
+            var target = new Math();
+
+            int expected = -5;
+            int actual = target.Subtract(3, 8);
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
